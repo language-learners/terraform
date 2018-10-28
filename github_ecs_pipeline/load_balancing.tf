7e3a7909f924d31498448ffccb04c72d2e0ce07d
Eric Kidd <git@randomhacks.net>
2018-10-28 16:09:11 -0400

    Document HTTPS setup & redirect HTTP
    This patch adds all the documentation comments I left out yesterday, and
    it also adds a redirect from HTTP to HTTPS.

--- github_ecs_pipeline/load_balancing.tf
+++ github_ecs_pipeline/load_balancing.tf
@@ -1,6 +1,9 @@
 # Register this container with our AWS "application load balancer", which can
 # serve multiple domains with certificates.
 
+# Set up a "target group". The listeners on our load balancer can send requests
+# to a target group, and an ECS service can register containers with this target
+# group.
 resource "aws_lb_target_group" "target_group" {
   name     = "${var.name}"
   port     = 80
@@ -8,21 +11,26 @@ resource "aws_lb_target_group" "target_group" {
   vpc_id   = "vpc-5abfab3c"
 }
 
+# Add a rule to our listener directing traffic to our target group whenever the
+# the domain matches.
 resource "aws_lb_listener_rule" "proxy" {
   listener_arn = "${var.listener_arn}"
   priority     = "${var.listener_rule_priority}"
 
+  # Send traffic to our target group.
   action {
     type             = "forward"
     target_group_arn = "${aws_lb_target_group.target_group.arn}"
   }
 
+  # Match based on hostname.
   condition {
     field  = "host-header"
     values = ["${var.host}"]
   }
 }
 
+# Create a certificate for our domain.
 resource "aws_acm_certificate" "cert" {
   domain_name       = "${var.host}"
   validation_method = "DNS"
@@ -32,6 +40,8 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+# Create a DNS record which shows that we control the domain, and authorize
+# validate of the certificate.
 resource "aws_route53_record" "cert_validation" {
   name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
   type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
@@ -40,11 +50,14 @@ resource "aws_route53_record" "cert_validation" {
   ttl     = 60
 }
 
+# Ask AWS to validate our certificate.
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = "${aws_acm_certificate.cert.arn}"
   validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
 }
 
+# Attach the certificate to our listener, so it can accept requests for that
+# domain.
 resource "aws_lb_listener_certificate" "cert" {
   listener_arn    = "${var.listener_arn}"
   certificate_arn = "${aws_acm_certificate_validation.cert.certificate_arn}"
--- load_balancer.tf
+++ load_balancer.tf
@@ -1,3 +1,8 @@
+# We use an AWS Application Load Balancer to direct traffic to our various
+# containers. This costs >=$21/month, but we tried various Docker proxy containers
+# which were more trouble than they're worth.
+
+# Our main load balancer, which handles all our websites.
 resource "aws_lb" "web_sites" {
   name               = "web-sites"
   internal           = false
@@ -6,13 +11,36 @@ resource "aws_lb" "web_sites" {
   subnets            = ["subnet-011dc549", "subnet-0f045d6a"]
 }
 
+# Set up a listener on port 80, but just redirect everything to HTTPS.
+resource "aws_lb_listener" "web_sites_http" {
+  load_balancer_arn = "${aws_lb.web_sites.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# Our HTTPS listener, which listens for HTTP requests. This doesn't do much by
+# itself; look for the aws_lb_listener_rule declarations to see how requests
+# actually get forwarded to the right server app.
 resource "aws_lb_listener" "web_sites_https" {
   load_balancer_arn = "${aws_lb.web_sites.arn}"
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2015-05"
+  # This certificate isn't really used for anything useful, but we need to
+  # specify one here.
   certificate_arn   = "${aws_acm_certificate_validation.language_learners.certificate_arn}"
 
+  # This default action really shouldn't matter, because we don't use it for
+  # anything.
   default_action {
     type = "redirect"
     redirect {
@@ -54,6 +82,8 @@ resource "aws_security_group" "load_balancer" {
   }
 }
 
+# Declare a dummy certificate so we have a default certificate for the listener.
+# Real certificates are attached to the aws_lb_listener_rule instead.
 resource "aws_acm_certificate" "language_learners" {
   domain_name       = "language-learners.org"
   validation_method = "DNS"
@@ -63,6 +93,9 @@ resource "aws_acm_certificate" "language_learners" {
   }
 }
 
+# Create a DNS record which proves that we control our domain and we want to
+# issue a certificate, allowing AWS Certificate Manager to validate our
+# certificate.
 resource "aws_route53_record" "language_learners_validation" {
   name    = "${aws_acm_certificate.language_learners.domain_validation_options.0.resource_record_name}"
   type    = "${aws_acm_certificate.language_learners.domain_validation_options.0.resource_record_type}"
@@ -71,6 +104,8 @@ resource "aws_route53_record" "language_learners_validation" {
   ttl     = 60
 }
 
+# Ask the AWS Certificate Manager to validate our certificate using the DNS
+# entry we created.
 resource "aws_acm_certificate_validation" "language_learners" {
   certificate_arn         = "${aws_acm_certificate.language_learners.arn}"
   validation_record_fqdns = ["${aws_route53_record.language_learners_validation.fqdn}"]
